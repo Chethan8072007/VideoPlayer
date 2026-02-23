ee007ce545e9b13d61a952490a6e0103da3e36e9
Chethan8072007 <Chethan8072007@gmail.com>
2026-02-23 22:04:48 +0530

    1

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <h1>VideoPlayer</h1>
+      </header>
+      <main className="app-main">
+        <div className="video-container">
+          <p>Video player content will go here</p>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
